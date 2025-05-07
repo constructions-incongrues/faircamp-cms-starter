9e7dc287d26d2edd5ff0cd07921ddaf42fe9d026
Tristan Rivoallan <tristan@rivoallan.net>
2025-05-08 01:07:19 +0200

    refactor: Update README title and define initial route in main.ts for CMS project

--- cms/README.md
+++ cms/README.md
@@ -1,4 +1,4 @@
-# cms-ts
+# cms
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- cms/src/main.ts
+++ cms/src/main.ts
@@ -4,7 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
-const routes = []
+const routes = [{ path: '/', component: App }]
 
 const router = createRouter({
   history: createWebHistory(),
