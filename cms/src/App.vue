b1a712b8ab9c8bf866c050709fff65d8224580e4
Tristan Rivoallan <tristan@rivoallan.net>
2025-05-08 03:28:38 +0200

    chore: Update package.json and pnpm-lock.yaml for version bump and dependency updates; modify App.vue to include commented import for faircamp-cms-config

--- cms/src/App.vue
+++ cms/src/App.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
+// import { setConfigProvider, getConfig } from '@constructions-incongrues/faircamp-cms-config'
 
-onMounted(async () => {
+onMounted(async () => {  
   // Dynamically import and run the UI package when this component is mounted
   await import('@constructions-incongrues/faircamp-cms-ui')
 })
